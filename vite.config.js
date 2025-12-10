b888da1092c206d227ed9a1d99d0684aa864d875
YEP <yep.work.it.gmail.com>
2025-12-10 03:50:06 +0200

    Make single html file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
     root: '.',
+    plugins: [viteSingleFile()],
     build: {
         outDir: 'dist',
         emptyOutDir: true,
